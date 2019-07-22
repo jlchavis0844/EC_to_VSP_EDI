83145226242e7cdf3cb50c4659123dcd883f9f35
James Chavis <jlchavis0844@hotmail.com>
2019-07-22 09:46:44 -0700

    Header object and interchange tracking done (no gui support)

--- EC_to_VSP_EDI/Header.cs
+++ EC_to_VSP_EDI/Header.cs
@@ -10,18 +10,27 @@ public class Header
     public const string SenderIDQualifier = "30";
     public const string SenderID = "94-2239786     ";
     public const string ReceiverIDQualifier = "30";
-    public const string ReceiverID = "94-1632821     ";
-    public static string InterchangeDate;
-    public static string InterchangeTime;
+    public const string ReceiverID = "94-1632821    ";
+    public string InterchangeDate;
+    public string InterchangeTime;
     public const char InterchangeControlID = '=';
     public const string InterchangeControlVersionNum = "00501";
-    public static string InterchangeControlNum;
+    public string InterchangeControlNum;
     public const char AcknowledgementRequested = '0';
     public const char UsageIndicator = 'P';
     public const char ComponentElementSeparator = '>';
     public const char SegmentTerminator = '~';
 
     public Header(){
-
+        InterchangeDate = InterchangeTracker.GetInterchangeDate();
+        InterchangeTime = InterchangeTracker.GetInterchangeTime();
+        InterchangeControlNum = InterchangeTracker.GetInterchangeNumber().ToString().PadLeft(9,'0');
 	}
+
+    public new string ToString() {
+        return SegementID + '*' + AuthorizationInfoQualifier + '*' + AuthorizationInfo + '*' + SecurityInfoQualifier + '*' +
+            SecurityInfo + '*' + SenderIDQualifier + '*' + SenderID + '*' + ReceiverIDQualifier + '*' + ReceiverID + '*' + InterchangeDate + '*' +
+            InterchangeTime + '*' + InterchangeControlID + '*' + InterchangeControlVersionNum + '*' + InterchangeControlNum + '*' +
+            AcknowledgementRequested + '*' + UsageIndicator + '*' + ComponentElementSeparator + SegmentTerminator;
+    }
 }
