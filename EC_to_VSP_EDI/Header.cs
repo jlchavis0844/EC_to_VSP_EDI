88c50ea1c7e6406bbdf8fe1213c8634271eb2d31
James Chavis <jlchavis0844@hotmail.com>
2019-09-26 14:28:13 -0700

    fixed ISA 08 Changed to padded right 15

--- EC_to_VSP_EDI/Header.cs
+++ EC_to_VSP_EDI/Header.cs
@@ -4,22 +4,22 @@ public class Header
 {
     private const string SegmentID                       = "ISA";
     private const string AuthorizationInfoQualifier      = "00";
-    private const string AuthorizationInfo               = "          ";
+    private readonly string AuthorizationInfo               = "".PadLeft(10);
     private const string SecurityInfoQualifier           = "00";
-    private const string SecurityInfo                    = "          ";
+    private readonly string SecurityInfo                 = "".PadLeft(10);
     private const string SenderIDQualifier               = "30";
-    private const string SenderID                        = "94-2239786     ";
+    private readonly string SenderID                        = "94-2239786".PadRight(15);//15 fixed width
     private const string ReceiverIDQualifier             = "30";
-    private const string ReceiverID                      = "94-1632821    ";
-    private string       InterchangeDate;
-    private string       InterchangeTime;
-    private const char   InterchangeControlID            = '=';
+    private readonly string ReceiverID                      = "94-1632821".PadRight(15);
+    private string InterchangeDate;
+    private string InterchangeTime;
+    private const char InterchangeControlID            = '=';
     private const string InterchangeControlVersionNum    = "00501";
-    private string       InterchangeControlNum;
-    private const char   AcknowledgementRequested        = '0';
-    private char         UsageIndicator;
-    private const char   ComponentElementSeparator       = '>';
-    private const char   SegmentTerminator               = '~';
+    private string InterchangeControlNum;
+    private const char AcknowledgementRequested        = '0';
+    private char UsageIndicator;
+    private const char ComponentElementSeparator       = '>';
+    private const char SegmentTerminator               = '~';
 
     public Header(string type){
         InterchangeDate = InterchangeTracker.GetInterchangeDate();
